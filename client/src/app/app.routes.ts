30577d0a70daeecb609a4922d84cbe8565fafc89
KiselevAndreyi22 <sid2233show@gmail.com>
2025-07-30 22:08:46 +0300

    add styles and components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { RegisterPageComponent} from './pages/authPages/registerPages/register-page.component';
+import { LoginPageComponent} from './pages/authPages/loginPages/login-page.component';
+import { LayoutComponent} from './layout/layout.component';
+import { DashboardComponent} from './features/dashboard/dashboard.component';
+import { ProjectsPageComponent} from './pages/authPages/productPages/product-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'profile', component: DashboardComponent },
+      { path: 'projects', component: ProjectsPageComponent },
+    ],
+  },
+  {
+    path: 'sign-up',
+    component: RegisterPageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+];
